be0c116974a7d904daddb9f2a617759a70015555
long <757099069@qq.com>
2018-06-23 17:31:45 +0800

    123

--- sele.py
+++ sele.py
@@ -4,8 +4,7 @@ from selenium.webdriver.support.select import Select
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait  #导入包
-from selenium.webdriver.support import expected_conditions as EC  #导入包
-from selenium.webdriver.common.by import By  #导入包
+
 
 browser=webdriver.Chrome()  #打开浏览器
 browser.get(r"C:\Users\zhazha\Desktop\alter.html")  #地址
